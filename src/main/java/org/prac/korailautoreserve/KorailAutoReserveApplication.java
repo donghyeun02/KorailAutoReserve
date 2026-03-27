5da991c1a9b6d154f23e7074a2b159d363a54582
신동현 <donghyeun02@gmail.com>
2026-03-27 21:19:22 +0900

    initial setting

---
+++ src/main/java/org/prac/korailautoreserve/KorailAutoReserveApplication.java
@@ -0,0 +1,12 @@
+package org.prac.korailautoreserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KorailAutoReserveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KorailAutoReserveApplication.class, args);
+    }
+}
